c1c299539d988c4ef900bd058d4d1ef48b4a62e0
sherawat42 <manishsherawat1@gmail.com>
2017-02-02 13:24:13 +0530

    user_authentications_added

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, except: [:index,:show]
   # GET /links
   # GET /links.json
   def index
@@ -14,7 +14,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +24,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
@@ -40,13 +40,15 @@ class LinksController < ApplicationController
   # PATCH/PUT /links/1
   # PATCH/PUT /links/1.json
   def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
+    if(current_user == @link.user)
+      respond_to do |format|
+        if @link.update(link_params)
+          format.html { redirect_to @link, notice: 'Link was successfully updated.' }
+          format.json { render :show, status: :ok, location: @link }
+        else
+          format.html { render :edit }
+          format.json { render json: @link.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -54,10 +56,16 @@ class LinksController < ApplicationController
   # DELETE /links/1
   # DELETE /links/1.json
   def destroy
-    @link.destroy
+    if(current_user == @link.user)
+      @link.destroy
+      respond_to do |format|
+        format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
+        format.json { head :no_content }
+        return
+      end
+    end
     respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to links_url, notice: "Invalid attempt to delete a link!" }
     end
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ActiveRecord::Base
+  belongs_to :user
+  validates :user_id, presence: true
 end
